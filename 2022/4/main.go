65bd5f8e0bc6bed09e34d2844a08220b6b0a1344
ismail simsek <ismailsimsek09@gmail.com>
2022-12-04 14:38:20 +0100

    very simple day 4 part 2 solution

--- 2022/4/main.go
+++ 2022/4/main.go
@@ -13,22 +13,17 @@ func main() {
 	defer closeFile()
 
 	fullyContainedPairsCount := 0
+	overlappingPairsCount := 0
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
 		fullyContainedPairsCount += FindContainedRanges(line)
+		overlappingPairsCount += FindOverlappingRanges(line)
 	}
-	fmt.Println(fullyContainedPairsCount)
+	fmt.Println(fullyContainedPairsCount, overlappingPairsCount)
 }
 
 func FindContainedRanges(ranges string) int {
-	elves := strings.Split(ranges, ",")
-	e1 := strings.Split(elves[0], "-")
-	e2 := strings.Split(elves[1], "-")
-
-	e1s, _ := strconv.ParseInt(e1[0], 10, 32)
-	e1e, _ := strconv.ParseInt(e1[1], 10, 32)
-	e2s, _ := strconv.ParseInt(e2[0], 10, 32)
-	e2e, _ := strconv.ParseInt(e2[1], 10, 32)
+	e1s, e1e, e2s, e2e := splitToElves(ranges)
 
 	if e1s <= e2s && e1e >= e2e {
 		return 1
@@ -37,4 +32,29 @@ func FindContainedRanges(ranges string) int {
 	}
 
 	return 0
+}
+
+func FindOverlappingRanges(ranges string) int {
+	e1s, e1e, e2s, e2e := splitToElves(ranges)
+
+	if e1s < e2s && e1e < e2s {
+		return 0
+	} else if e1s > e2s && e2e < e1s {
+		return 0
+	}
+
+	return 1
+}
+
+func splitToElves(ranges string) (e1s, e1e, e2s, e2e int64) {
+	elves := strings.Split(ranges, ",")
+	e1 := strings.Split(elves[0], "-")
+	e2 := strings.Split(elves[1], "-")
+
+	e1s, _ = strconv.ParseInt(e1[0], 10, 32)
+	e1e, _ = strconv.ParseInt(e1[1], 10, 32)
+	e2s, _ = strconv.ParseInt(e2[0], 10, 32)
+	e2e, _ = strconv.ParseInt(e2[1], 10, 32)
+	// return e1s, e1e, e2s, e2e
+	return
 }
--- 2022/4/main_test.go
+++ 2022/4/main_test.go
@@ -24,3 +24,28 @@ func TestFindContainedRanges(t *testing.T) {
 		}
 	}
 }
+
+func TestFindOverlappingRangespingRanges(t *testing.T) {
+	cases := []struct {
+		line     string
+		expected int
+	}{
+		{line: "2-8,3-7", expected: 1},
+		{line: "5-7,7-9", expected: 1},
+		{line: "6-6,4-6", expected: 1},
+		{line: "2-6,4-8", expected: 1},
+		{line: "2-6,7-8", expected: 0},
+		{line: "1-1,2-2", expected: 0},
+		{line: "1-5,8-9", expected: 0},
+		{line: "68-69,45-69", expected: 1},
+		{line: "68-69,45-67", expected: 0},
+	}
+
+	for _, c := range cases {
+		point := FindOverlappingRanges(c.line)
+
+		if point != c.expected {
+			t.Error(fmt.Sprintf("Line: %s failed. Expected: %d but got: %d", c.line, c.expected, point))
+		}
+	}
+}
